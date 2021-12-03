0646355889f7610e5783a749b94fb4680f40ba09
bealabea <beatricemarin89@yahoo.it>
2021-12-03 14:47:08 +0100

    Js train ticket instructions

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// creo una variabile che ha come valore l'oggetto html che richiama l'id attribuito
+
+//chiedo l'età all'utente usando prompt
+
+//chiedo quanti km deve fare l'utente usando prompt
+
+
+//creo una variabile per il prezzo pieno a cui indico di non superare i due decimali
+//creo una variabile per il prezzo scontato del 20% a cui indico di non superare i due decimali
+//creo una variabile per il prezzo scontato del 40% a cui indico di non superare i due decimali
+
+
+//creo un ciclo di if: se l'utente ha meno di 18 anni avrà lo sconto del 20%,
+//altrimenti se avrà più di 65 anni avrà lo sconto del 40%,
+//altrimenti pagherà prezzo pieno.
+
+//inserisco il simbolo dell'euro nell'html
